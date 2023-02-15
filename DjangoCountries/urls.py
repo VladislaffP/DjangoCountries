93e147afb1e91f47c63685b08e65ccaf3d95d377
VVP <vladislav_p@mail.ru>
2023-02-15 23:40:14 +0300

    занятие 2. Добавление шаблонов

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('country/<path:id>/', views.view_country),
+    path('countries-list', views.countries_list),
+    path('languages', views.langs)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseNotFound
+import json
 
 def home(request):
-    result = """
-    <h1>"Проект DjangoCountries"</h1>
-    <strong>Исполнитель</strong>: <i>Паницкий В.В.</i>
-    """
-    return HttpResponse(result)
+    context = {
+        "pr_name": 'Проект DjangoCountries',
+        "creator": "Паницкий В.В."
+    }
+    return render(request, 'index.html', context)
+
+
+def countries_list(request):
+    with open('countries.json') as f:
+        cn = json.load(f)
+    context = {
+        "cnts": cn
+    }
+    return render(request, 'countries-list.html', context)
+
+def view_country(request, id):
+    with open('countries.json') as f:
+        cn = json.load(f)
+    for c in cn:
+        if c['country'] == id:
+            context = {
+                "name": c['country'],
+                "lang": c['languages']
+            }
+    return render(request, 'country.html', context)
+#    result = """
+#    <h1>"Проект DjangoCountries"</h1>
+#    <strong>Исполнитель</strong>: <i>Паницкий В.В.</i>
+#    """
+#    return HttpResponse(result)
+
+def langs(request):
+    langs = []
+    with open('countries.json') as f:
+        cn = json.load(f)
+    for l1 in cn:
+        for l2 in l1['languages']:
+            if l2 not in langs:
+                langs.append(l2)
+    langs.sort()
+    context = {
+        'lang': langs
+    }
+    return render(request, 'languages.html', context)
+
+
